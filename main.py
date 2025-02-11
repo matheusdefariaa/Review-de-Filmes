a87c1c895c130d1388b08ac29b4a1310a81798d6
matheus de faria <mattheusdefaria@proton.me>
2025-02-11 11:07:06 -0300

    Atualização

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.mount("/static",StaticFiles(directory='static'),name='static')
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def home(request:Request):
+def home(request:Request):
     return templates.TemplateResponse(
         request=request,name="index.html"
     )
